ede8dba1357a9f78b681420fcb69350c39c8c9ce
Kashif Aslam <mkashif.aslam36@gmail.com>
2024-12-08 20:36:40 +0500

    add interfaces and generics examples

--- sections/interfaces-generics/main.go
+++ sections/interfaces-generics/main.go
@@ -9,7 +9,30 @@ import (
 	"strings"
 )
 
+type saver interface {
+	Save() error
+}
+
+type outputter interface {
+	saver
+	Display()
+}
+
 func main() {
+
+	//printSomething(1)
+	//printSomething(2.5)
+	//printSomething("hello!")
+	//
+	//intResult := add(1, 2)
+	//fmt.Println(intResult)
+	//
+	//floatResult := add(1.3, 2.6)
+	//fmt.Println(floatResult)
+	//
+	//strResult := add("hello,", "world")
+	//fmt.Println(strResult)
+
 	title, content := getNoteData()
 
 	userNote, err := note.New(title, content)
@@ -21,30 +44,77 @@ func main() {
 	todoText := getTodoData()
 	userTodo, err := todo.New(todoText)
 
+	//printSomething(userTodo)
+
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 
-	userTodo.Display()
-	err = userTodo.Save()
+	err = outputData(userTodo)
+
+	if err != nil {
+		return
+	}
+
+	err = outputData(userNote)
+
+	if err != nil {
+		return
+	}
+}
+
+func add[T int | float64 | string](a, b T) T {
+	return a + b
+}
+
+func printSomething(value any) {
+	intVal, ok := value.(int)
+
+	if ok {
+		fmt.Println("Integer: ", intVal)
+		return
+	}
+
+	floatVal, ok := value.(float64)
+
+	if ok {
+		fmt.Println("Float: ", floatVal)
+		return
+	}
+
+	stringVal, ok := value.(string)
+
+	if ok {
+		fmt.Println("String: ", stringVal)
+		return
+	}
+
+	//switch value.(type) {
+	//case int:
+	//	fmt.Println("Integer: ", value)
+	//case float64:
+	//	fmt.Println("Float: ", value)
+	//case string:
+	//	fmt.Println(value)
+	//}
+}
+
+func outputData(data outputter) error {
+	data.Display()
+	return saveData(data)
+}
+
+func saveData(data saver) error {
+	err := data.Save()
 
 	if err != nil {
 		fmt.Println("Saving the todo failed.")
-		return
+		return err
 	}
 
-	fmt.Println("Saving the note succeeded!")
-
-	userNote.Display()
-	err = userNote.Save()
-
-	if err != nil {
-		fmt.Println("Saving the note failed.")
-		return
-	}
-
-	fmt.Println("Saving the note succeeded!")
+	fmt.Println("Saving the todo succeeded!")
+	return nil
 }
 
 func getTodoData() string {
