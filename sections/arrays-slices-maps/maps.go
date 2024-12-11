aa8f277b6581002e4e5e86a9da06316601e2eb42
Kashif Aslam <mkashif.aslam36@gmail.com>
2024-12-11 11:45:49 +0500

    add maps

--- sections/arrays-slices-maps/maps.go
+++ sections/arrays-slices-maps/maps.go
@@ -3,5 +3,15 @@ package main
 import "fmt"
 
 func Maps() {
-	fmt.Println("Maps")
+	websites := map[string]string{
+		"Google":              "https://www.google.com",
+		"Amazon Web Services": "https://www.aws.com",
+	}
+	fmt.Println(websites)
+
+	websites["Linkedin"] = "https://www.linkedin.com"
+	fmt.Println(websites)
+
+	delete(websites, "Google")
+	fmt.Println(websites)
 }
