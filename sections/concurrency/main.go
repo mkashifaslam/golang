abdad3a2fc95ee51cc08650ce2b6747929c5654b
Kashif Aslam <mkashif.aslam36@gmail.com>
2024-12-15 13:06:51 +0500

    add simple goroutine example

--- sections/concurrency/main.go
+++ sections/concurrency/main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
-	fmt.Println("Concurrency!")
+	done := make(chan bool)
+	go greet("Greeting # 1", done)
+	go greet("Greeting # 2", done)
+	go slowGreet("Greeting # 4 - slow", done)
+	go greet("Greeting # 3", done)
+
+	for range done {
+	}
+}
+
+func greet(phrase string, doneChan chan bool) {
+	fmt.Println("Hello!", phrase)
+	doneChan <- true
+}
+
+func slowGreet(phrase string, doneChan chan bool) {
+	time.Sleep(time.Second * 3)
+	fmt.Println("Hello!", phrase)
+	doneChan <- true
+	close(doneChan)
 }
