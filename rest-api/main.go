713856b674ca44ba72fb89f59a34ed80285498f9
Kashif Aslam <mkashif.aslam36@gmail.com>
2024-12-16 11:01:43 +0500

    add gin framework package and setup first http route

--- rest-api/main.go
+++ rest-api/main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Rest API Server!")
+	server := gin.Default()
+
+	server.GET("/events", getEvents)
+
+	server.Run(":8080")
+}
+
+func getEvents(context *gin.Context) {
+	context.JSON(http.StatusOK, gin.H{
+		"message": "successfully received request",
+	})
 }
