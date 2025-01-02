1063c5b7ce873597e06f496239ca5f852d64a831
Kashif Aslam <mkashif.aslam36@gmail.com>
2025-01-02 12:25:56 +0500

    add comments on routes groups

--- rest-api/routes/routes.go
+++ rest-api/routes/routes.go
@@ -6,20 +6,22 @@ import (
 )
 
 func RegisterRoutes(server *gin.Engine) {
-	// events
+	// events routes
 	server.GET("/events", getEvents)
 	server.GET("/events/:id", getEvent)
 
+	// auth middleware
 	authenticated := server.Group("/")
 	authenticated.Use(middlewares.Authenticate)
 
+	// auth routes
 	authenticated.POST("/events", createEvent)
 	authenticated.PUT("/events/:id", updateEvent)
 	authenticated.DELETE("/events/:id", deleteEvent)
 	authenticated.POST("/events/:id/register", registerForEvent)
 	authenticated.DELETE("/events/:id/register", cancelRegistration)
 
-	// users
+	// users routes
 	server.POST("/signup", signup)
 	server.POST("/login", login)
 }
