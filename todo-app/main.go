1f66f42f6a50a9c96363ea2bf786a93c5f3d5932
Kashif Aslam <mkashif.aslam36@gmail.com>
2025-01-02 11:07:59 +0500

    add help and exit commands in welcome message

--- todo-app/main.go
+++ todo-app/main.go
@@ -10,6 +10,8 @@ func main() {
 	fmt.Println("Welcome Todos app")
 	fmt.Println("----------------------")
 	fmt.Printf("\nCommands:" +
+		"\nhelp get help" +
+		"\nexit exit app\n" +
 		"\ntasks add <title>" +
 		"\ntasks list" +
 		"\ntasks find <taskId>" +
