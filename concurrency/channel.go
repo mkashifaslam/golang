6407cd93f8258c93d7817b9ac8aa41164253edb4
Kashif Aslam <mkashif.aslam36@gmail.com>
2025-01-16 11:05:27 +0500

    add sync wait example of currency management

--- concurrency/channel.go
+++ concurrency/channel.go
@@ -3,19 +3,31 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"sync"
 	"time"
 )
 
 func Channel() {
+
+	var wg sync.WaitGroup
+
 	num := 5
 
 	ch := make(chan int)
 
-	sendChanValue(ch, num)
+	sendChanValue(ch, num, &wg)
 
 	//receiveChanWithSelect(ch)
 
-	receiveChanWithFor(ch, num)
+	//receiveChanWithFor(ch, num)
+
+	//receiveChanWithSelect(ch)
+
+	receiveChanWithWait(ch, num, &wg)
+
+	wg.Wait()
+
+	fmt.Println("All goroutines done")
 
 	//time.Sleep(time.Duration(rand.Intn(100)) * time.Millisecond)
 
@@ -23,10 +35,11 @@ func Channel() {
 
 }
 
-func sendChanValue(ch chan int, num int) {
+func sendChanValue(ch chan int, num int, wg *sync.WaitGroup) {
 	for i := 1; i <= num; i++ {
+		wg.Add(1)
 		go func(i int) {
-			fmt.Printf("goroutine output %d\n", i)
+			fmt.Printf("goroutine input %d\n", i)
 			ch <- i
 		}(i)
 	}
@@ -52,4 +65,14 @@ func receiveChanWithSelect(ch chan int) {
 			return
 		}
 	}
+}
+
+func receiveChanWithWait(ch chan int, num int, wg *sync.WaitGroup) {
+	for i := 1; i <= num; i++ {
+		go func() {
+			defer wg.Done()
+			fmt.Println("goroutine output", <-ch)
+		}()
+	}
+
 }
