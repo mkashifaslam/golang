06511185c18c1459615e566d218aad7e77682020
Kashif Aslam <mkashif.aslam36@gmail.com>
2025-01-18 10:38:25 +0500

    add separator "," to manage complex string values

--- str2num/lib/main.go
+++ str2num/lib/main.go
@@ -4,19 +4,39 @@ import (
 	"strings"
 )
 
+var (
+	mSep = ","
+	sSep = " "
+)
+
 func StrToNumber(str string) int64 {
-	strValues := strings.Split(str, " ")
+	var strValues = make([]string, 0)
+
+	if strings.Contains(str, mSep) {
+		strValues = strings.Split(str, mSep)
+	} else {
+		strValues = append(strValues, str)
+	}
+
 	numMap := GetNumbersDict()
 
+	var result int64
+	for _, strValue := range strValues {
+		result += calcResult(strings.Split(strValue, sSep), numMap)
+	}
+
+	return result
+}
+
+func calcResult(strValues []string, numMap Str2Num) int64 {
 	var result int64
 	var carry int64
 	for _, strValue := range strValues {
 		var strVal = strings.ToLower(strValue)
 		var num = numMap[strings.ToLower(strValue)]
+
 		if GetRank(Last, strVal) {
 			carry *= num
-			result += carry
-			carry = 0
 		} else {
 			carry += num
 		}
